031dbd61adb8775a3672cfc0d5b42b0accc94a63
Putra Prima Arhandi <putraprima@gmail.com>
2023-12-10 06:09:47 +0700

    counter app cubit

---
+++ lib/cubit/counter_cubit.dart
@@ -0,0 +1,17 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'counter_state.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterState(counterValue: 0));
+
+  void increment() => emit(
+        CounterState(counterValue: state.counterValue + 1),
+      );
+  void decrement() {
+    if (state.counterValue > 0) {
+      emit(CounterState(counterValue: state.counterValue - 1));
+    }
+  }
+}
